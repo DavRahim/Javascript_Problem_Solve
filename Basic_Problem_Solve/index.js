8fa8a75d03b3af9351d66c44c39ceb7f4badf8d8
Abdur Rahim <mohammadabdurrahim1966@gmail.com>
2025-07-20 23:24:18 +0600

    3 problem solve

---
+++ Basic_Problem_Solve/index.js
@@ -0,0 +1,41 @@
+// Beginner problem solve
+
+// TODO:✅ একটি সংখ্যা জোড় না বিজোড় তা বের করো
+function isEvenOrOdd(num) {
+  if (typeof num !== "number") return "Please enter a valid number";
+  if (num === 0) return "Zero";
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+// console.log(`isEvenOrOdd:`, isEvenOrOdd(3));
+
+// TODO: ✅ ১ থেকে ১০০ পর্যন্ত সংখ্যা প্রিন্ট করো;
+
+function printNumbers() {
+  let numbers = [];
+  for (let i = 1; i <= 100; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+// console.log(`printNumbers:`, printNumbers());
+
+// TODO:✅ ১ থেকে ১০০ পর্যন্ত শুধু জোড় সংখ্যা প্রিন্ট করো
+
+function printEvenNumbers() {
+  let evenNumber = [];
+  for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+      if (i === 0) continue; // Skip zero if you want to exclude it
+      evenNumber.push(i);
+    }
+  }
+
+  return evenNumber;
+}
+
+console.log(`printEvenNumbers:`, printEvenNumbers());
